b898ab61785964f01323e9faa570ed2936955363
Cristian Cáceres <ccaceresm1@miumg.edu.gt>
2023-06-08 15:40:48 -0600

    creacion

---
+++ src/main/java/com/CDRP/core/CoreVentaApplication.java
@@ -0,0 +1,13 @@
+package com.CDRP.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreVentaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreVentaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CDRP/core/CoreVentaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CDRP.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreVentaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
